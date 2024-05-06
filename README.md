77aa46f14590369a54253192e4c43a2de4674b79
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2024-05-06 16:36:08 -0400

    Initial Commit: second take
    go gaggle on some icecream

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Ferremas_Api
-Php is our friend not Food
+# PHP REST API
+La mismisima que nos dio el profe la segunda clase de API solo que esta tiene 2 modelos semi-funcionales(explicare mas abajo) y escalable, no tiene checks para evitar problemas con los cuerpos de los requests pero eso sera para otra ver. mas adelante 
+
+### Models
+- existen 2 modelos, Students y Productos, ambos estan conectados a la misma tabla, esto se puede cambiar en sus respectivos documentos, los cuales tienen que reflejar su tabla en la BDD
+
+### BDD
+- La BDD esta programada(tablas creadas) en sql, solo hay que crear las otras tablas, me gustaria usar una BDD en sqlite3 en vez de mySQl por que es mas facil compartir las tablas creadas
+
+### Config
+- en caso de migrar a sqlite3 el archivo completo debera cambiar ya que la conexion entre php y sqlite3 es diferente a mysql, esto porque no necesitamos un servidor
+---
+Esto es todo lo que tenia que decir, es mas facil escribirlo que hablarlo - pero si me piden escribir minimo 3 lineas en una prueba no puedo, honestly can some1 explain that to me? 
---
+++ php_api/api/service.php
@@ -0,0 +1,105 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+	header('Access-Control-Allow-Methods: POST');
+
+	include_once '../config/Database.php';
+	include_once '../models/Student.php';
+
+	
+
+	$db = new Database();
+	$db = $db->connect();
+
+	// dynamic model| now only thing is differentiating the params of each
+	$model = $_GET['model'];
+	switch ($model) {
+		case 'Student':
+			$model = new Student($db);
+			break;
+		case 'Producto':
+			$model = new Producto($db);
+			break;
+		default:
+			echo json_encode(array('message' => "$model doesn't exist!"));
+			exit;
+	}
+	if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+		$data = json_decode(file_get_contents("php://input"));
+		$model->id = isset($data->id) ? $data->id : NULL;
+		if(! is_null($model->id)) {
+			if($model->delete()) {
+			echo json_encode(array('message' => 'Student deleted'));
+			} else {
+			echo json_encode(array('message' => 'Student Not deleted, try again!'));
+			}
+		} else {
+		echo json_encode(array('message' => "Error: Student ID is missing!"));
+		}
+	}
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$data =(array) json_decode(file_get_contents("php://input"));
+
+		foreach(get_object_vars($model) as $k=>$v){
+			if ($k!="id"){
+				$model->$k = $data[$k];
+			}
+		}
+		if($model->postData()) {
+			echo json_encode(array('message' => 'Student added'));
+		} else {
+			echo json_encode(array('message' => 'Student Not added, try again!'));
+		}
+	}
+	if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+		$data =(array) json_decode(file_get_contents("php://input"));
+		try {
+			foreach(get_object_vars($model) as $k=>$v){
+				$model->$k = $data[$k];
+			}
+			if(! is_null($model->id)) {
+				if($model->putData()) {
+				echo json_encode(array('message' => 'Student updated'));
+				} else {
+				echo json_encode(array('message' => 'Student Not updated, try again!'));
+				}
+			} else {
+			echo json_encode(array('message' => "Error: Student ID is missing!"));
+			}
+		} catch (\Throwable $th) {
+			echo $th;
+		}
+	}
+	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+		try {
+			$res = $model->fetchAll();
+			$resCount = $res->rowCount();
+			$models_ = array();
+			$data = json_decode(file_get_contents("php://input"));
+			if(isset($data->id)) {
+				$model->id = $data->id;
+				if($model->fetchOne()) {
+					foreach ($model as $k => $v) {
+						$models_[$k]=$v;
+					}
+					print_r(json_encode($models_));
+				} else {
+					echo json_encode(array('message' => "No records found!"));
+				}
+			} elseif($resCount > 0) {
+				$models = array();
+				while($row = $res->fetch(PDO::FETCH_ASSOC)) {
+					foreach($row as $k => $v){
+						$models_[$k]=$v;
+					}
+					array_push($models, $models_);
+				}
+				echo json_encode($models);
+			} else {
+				echo json_encode(array('message' => "No records found!"));
+			}
+		}catch (Throwable $th) {
+			//throw $th;
+			echo $th;
+		}
+	} 
